c8db9796125731e143cdd06e0f90d10a2c7e6cbc
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-09-18 19:30:05 -1200

    Add project files.

---
+++ WebStoreApi/Services/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebStoreApi.Services
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+    }
+}
