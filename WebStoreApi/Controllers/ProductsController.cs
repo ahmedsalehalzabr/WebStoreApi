3a42bb6c91ad719b928cb082610c651b25588220
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-09-25 01:05:05 -1200

    post product controller save the image on the server and save product in the database

--- WebStoreApi/Controllers/ProductsController.cs
+++ WebStoreApi/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebStoreApi.Model;
 using WebStoreApi.Services;
 
 namespace WebStoreApi.Controllers
@@ -10,10 +11,12 @@ namespace WebStoreApi.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly AppDbContext context;
+        private readonly IWebHostEnvironment env;
 
-        public ProductsController(AppDbContext context)
+        public ProductsController(AppDbContext context,IWebHostEnvironment env)
         {
             this.context = context;
+            this.env = env;
         }
 
         [HttpGet]
@@ -31,6 +34,44 @@ namespace WebStoreApi.Controllers
             {
                 return NotFound();
             }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromForm]ProductDto productDto)
+        {
+            if(productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "Teh ImageFile is Requird");
+                return BadRequest(ModelState);
+            }
+
+            // save the image on the server
+            string imageFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            imageFileName += Path.GetExtension(productDto.ImageFile.FileName);
+
+            string imagesFolder = env.WebRootPath + "/images/products";
+
+            using (var stream =System.IO.File.Create(imagesFolder + imageFileName))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
+
+            // save product in the database
+            Product product = new Product()
+            {
+                Name = productDto.Name,
+                Brand = productDto.Brand,
+                Category = productDto.Category,
+                Price = productDto.Price,
+                Description = productDto.Description ?? "",
+                ImageFileName = imageFileName,
+                CreatedAt = DateTime.Now,
+            };
+             
+            await context.Products.AddAsync(product);
+            await context.SaveChangesAsync();
 
             return Ok(product);
         }
