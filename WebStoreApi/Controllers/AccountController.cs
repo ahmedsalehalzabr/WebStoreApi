e5da5e6cdc8df5db4cf283b06110ff1c522d13e4
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-09-25 07:04:07 -1200

    solve problem in Account

--- WebStoreApi/Controllers/AccountController.cs
+++ WebStoreApi/Controllers/AccountController.cs
@@ -22,7 +22,7 @@ namespace WebStoreApi.Controllers
             this.context = context;
         }
 
-        [HttpPost]
+        [HttpPost("Register")]
         public IActionResult Register(UserDto userDto)
         {
             var emailCount =  context.Users.Count(u => u.Email == userDto.Email);
@@ -73,6 +73,50 @@ namespace WebStoreApi.Controllers
             };
             return Ok(response);
         }
+
+        [HttpPost("Login")]
+        public IActionResult Login(string email, string password)
+        {
+            var user = context.Users.FirstOrDefault(x => x.Email == email);
+            if (user == null)
+            {
+                ModelState.AddModelError("Error", "email or password not valid");
+                return BadRequest(ModelState);
+            }
+
+            // verify the password
+            var passHash = new PasswordHasher<User>();
+            var result = passHash.VerifyHashedPassword(new Model.User(), user.Password, password);
+            if (result == PasswordVerificationResult.Failed)
+            {
+                ModelState.AddModelError("Password", "wrong password");
+                return BadRequest(ModelState);
+            }
+
+            var jwt = CreateJwToken(user);
+
+            UserProfileDto profile = new UserProfileDto()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Phone = user.Phone,
+                Address = user.Address,
+                Role = user.Role,
+                CreateAt = DateTime.Now,
+            };
+
+            var response = new
+            {
+                Token = jwt,
+                User = profile
+            };
+            return Ok(response);
+        }
+
+
+
         private string CreateJwToken(User user)
         {
             List<Claim> claims = new List<Claim>()
@@ -83,7 +127,7 @@ namespace WebStoreApi.Controllers
 
             string strKey = configuration["JwtSettings:Key"]!;
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(strKey));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
             var token = new JwtSecurityToken(
                 issuer: configuration["JwtSettings:Issuer"],
                 audience: configuration["JwtSettings:Audience"],
