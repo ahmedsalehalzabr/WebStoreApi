e8dd48b2c6e420f644bed7df9252dcc391873746
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-09-26 04:33:01 -1200

    Get Cart

---
+++ WebStoreApi/Model/CartDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebStoreApi.Model
+{
+    public class CartDto
+    {
+        public List<CartItemDto> CartItem { get; set; } = new();
+        public decimal SubTotal { get; set; }
+        public decimal ShippingFee { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ WebStoreApi/Model/CartItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebStoreApi.Model
+{
+    public class CartItemDto
+    {
+        public Product Product { get; set; } 
+        public int Quantity { get; set; }
+    }
+}
